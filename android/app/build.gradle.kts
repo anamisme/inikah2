eff431592e927dc9a422f7a32085972811277c70
Baitul Hikmah <baitulhikmah@Baituls-MacBook-Air.local>
2026-07-30 19:40:17 +0700

    version 19.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nucleapp.na_76ba41"
         minSdk = 24
         targetSdk = 36
-        versionCode = 19
-        versionName = "18.1"
+        versionCode = 20
+        versionName = "19.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
